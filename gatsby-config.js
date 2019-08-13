a4809f0cd6173fcbe7d3ed89a717daa2208e8516
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2019-08-13 06:49:32 -0300

    Add suport to code lighlight with prismjs plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
               maxWidth: 750,
               linkImagesToOriginal: false
             }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              showLineNumbers: false,
+              noInlineHighlight: false
+            }
           }
         ]
       }
@@ -42,9 +50,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
--- src/posts/ola-mundo/ola-mundo.md
+++ src/posts/ola-mundo/ola-mundo.md
@@ -4,3 +4,23 @@ date: "2019-08-12"
 ---
 
 Meu primeiro post!
+
+Imagem: 
+
+![code](./code.png)
+
+Code snippet
+
+Também com suporte a `console.log` snippet
+
+```javascript
+console.log('olá mundo!');
+```
+
+```html
+<html>
+    <body>
+        <h1>Olá mundo!</h1>
+    </body>
+</html>
+```
