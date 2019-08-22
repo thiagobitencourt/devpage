f71f561b4a50eab2650738a099b196d486b94f7d
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2019-08-22 06:26:18 -0300

    Changes favicon and theme color for mobile devices

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,10 +45,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2ECC71`,
+        theme_color: `#2ECC71`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/blog-icon.png`, // This path is relative to the root of the site.
       },
     }
   ],
