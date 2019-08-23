fa1b845a3e41d46440bd7f553c176de35d47ef04
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2019-08-23 06:50:30 -0300

    Update blockquote style

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "../../styles/reset.scss";
 import "../../styles/index.scss";
 import "../../styles/theme.scss";
 
